a6c18a52cd3c4666e8b9e423f10ce422c61057a3
jinhyeok <aqwstn@naver.com>
2024-12-08 04:24:36 +0900

    add a code for LLM Based  emotion analysis

--- diary/views.py
+++ diary/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
-from .models import Diary
+from .models import Diary, EmotionAnalysis
 from .forms import DiaryForm
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.urls import reverse
 import json
+from .classifier_model import BERTClassifier
+import torch
+from transformers import BertModel
+from kobert_tokenizer import KoBERTTokenizer
+
+bertmodel = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False)
+model = BERTClassifier(bertmodel, hidden_size=768, num_classes=6, dr_rate=0.5)
+save_path = "./bert_classifier_model.pt"
+state_dict = torch.load(save_path, map_location=torch.device('cpu'))
+model.load_state_dict(state_dict)
+model.eval()
+print("Model loaded successfully")
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+
+emotions = ('anger', 'sadness', 'anxiety', 'hurt', 'embarrassment', 'happiness')
 
 @login_required(redirect_field_name='login')
 def diary_write_view(request):
@@ -22,6 +37,24 @@ def diary_write_view(request):
             diary = form.save(commit=False)
             diary.user = request.user.user
 
+            content = form.cleaned_data['content']
+            emotion_scores = predict_emotion(content)
+
+            max_emotion_idx = emotion_scores.index(max(emotion_scores))
+            max_emotion = emotions[max_emotion_idx]
+
+            emotion_analysis = EmotionAnalysis.objects.create(
+                anger_score=emotion_scores[0],
+                sadness_score=emotion_scores[1],
+                anxiety_score=emotion_scores[2],
+                hurt_score=emotion_scores[3],
+                embarrassment_score=emotion_scores[4],
+                happiness_score=emotion_scores[5],
+                summary = max_emotion,
+                recommend_action="기본 추천"
+            )
+            # Diary와 EmotgionAnalysis 연결
+            diary.emotion_analysis = emotion_analysis
             diary.save()
             messages.success(request, '일기가 성공적으로 저장되었습니다.')
             return redirect('diary')
@@ -100,4 +133,18 @@ def diary_detail_view(request, diary_id):
         'diary' : diary,
     }
     return render(request, 'diary/diary_detail.html', context)
+
+
+def predict_emotion(content):
+    inputs = tokenizer(content, return_tensors='pt', padding=True, max_length=256)
+    token_ids = inputs['input_ids']
+    attention_mask = inputs['attention_mask']
+    token_type_ids = inputs['token_type_ids']
+
+    with torch.no_grad():
+        output = model(token_ids, attention_mask.sum(dim=1), token_type_ids)
+
+    probabilities = torch.nn.functional.softmax(output, dim=1).squeeze().tolist()
+    scores = [round(prob * 10, 2) for prob in probabilities]
+    return scores
 
